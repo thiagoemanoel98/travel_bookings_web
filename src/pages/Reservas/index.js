58b83fa0dfd5d6acdb30dba248511d850af93955
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-06-11 17:22:14 -0300

    Style: Pagina Reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { MdDelete } from "react-icons/md";
+
+import './style.css'
 
 export default function Reservas() {
   return (
     <div>
-      <h1>Reservssssssssssssssssssas</h1>
+      <h1 className="title">Você solicitou 1 reservas</h1>
+
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="Maceio"
+        />
+        <strong>Viagem Macio 7 Dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+
+      <footer>
+        <button type="button">Solicitar reservas</button>
+      </footer>
     </div>
   );
 }

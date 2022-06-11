835ff3ba28deb3625df694ed39eab503fc38d1c8
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-06-11 18:06:41 -0300

    Configurando Reducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import MyRoutes from "./routes";
 import Header from "./components/Header";
 
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <MyRoutes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <MyRoutes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
